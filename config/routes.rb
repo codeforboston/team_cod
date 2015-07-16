7992bb3e9ef15427cbbe13f1b4137c2e259112c7
Robert Thau <rst@ai.mit.edu>
2015-07-16 17:52:43 -0400

    Disable standard Devise user sign-up.
    Devise makes it strangely difficult to do this and still allow
    password changes through the web (they're on the same controller).
    Strange routes.rb incantations, and dinks to the pw-change form
    itself, from their Github wiki at:
    
    https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-edit-their-password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
+  # The following are needed to get access to 'change password', without
+  # allowing full registration.  Sigh...
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    patch 'users/:id' => 'devise/registrations#update', :as => 'user_registration'            
+  end 
   resources :users
   resources :contact
   resources :listings, controller: 'events', as: 'events' do
